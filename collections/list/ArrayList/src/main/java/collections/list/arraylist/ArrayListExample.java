97d11f98db939314bf62fc76fa8bf5e43afa7b65
SunJunipero <tkonstantin1381@gmail.com>
2018-05-14 10:31:46 +0300

    add collections samples

--- collections/list/ArrayList/src/main/java/collections/list/arraylist/ArrayListExample.java
+++ collections/list/ArrayList/src/main/java/collections/list/arraylist/ArrayListExample.java
@@ -1,4 +1,17 @@
 package collections.list.arraylist;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ArrayListExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 1; i <= 10; i++) {
+            list.add(random.nextInt(i));
+        }
+        System.out.println(list);
+    }
 }
---
+++ collections/src/main/java/CollectionExample.java
@@ -0,0 +1,62 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class CollectionExample {
+    public static void main(String[] args) {
+        Collection<Integer> collection  = new ArrayList<>();
+        List<String> strings;
+        Set<String> set;
+        Queue<String> queue;
+        Map<String, String> map = new HashMap<>();
+
+        /*
+         * Add
+         */
+        System.out.println("Add");
+        collection.add(1);
+        collection.add(2);
+        System.out.println(collection.size());
+        System.out.println(collection);
+
+        /*
+         * Remove
+         */
+        System.out.println("Remove");
+        collection.remove(3);
+        collection.remove(2);
+        System.out.println(collection.size());
+        System.out.println(collection);
+
+        /*
+        walk through iterator
+         */
+        System.out.println("Walk through iterator");
+        Integer [] integers = new Integer[]{1,3,4,5,6};
+        collection.addAll(Arrays.asList(integers));
+        Iterator<Integer> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        /*
+        walk through foreach
+         */
+        System.out.println("Walk through foreach");
+        for (Integer integer : collection) {
+            System.out.println(integer);
+        }
+
+        //TODO Map quiz
+        /*
+        add null keys in map
+         */
+        System.out.println("map quiz");
+        map.put(null,"1");
+        map.put(null,"2");
+        map.put(null,"3");
+        map.put(null,null);
+
+        System.out.println(map.get(null));
+
+
+    }
+}
